c8bb71b6e02d6aeead0b9ae9baa8b0058f4e2e9d
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-10-05 17:52:34 +0300

    feat: add work 5

---
+++ Work-5/index.js
@@ -0,0 +1,37 @@
+/*
+Kendi Web Sunucumuzu yazalım.
+Kendi bilgisayarımızda aşağıdaki özellikleri kullanarak sunucumuzu yazalım.
+
+createServer metodunu kullanacağız.
+index, hakkimda ve iletisim sayfaları oluşturalım.
+Sayfalara içerik olarak xxx sayfasına hoşgeldiniz şeklinde h2 başlıkları yazdıralım.
+port numarası olarak 5000'i kullanalım.
+*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>Index sayfasina hos geldiniz...</h2>");
+    } else if (url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>Hakkimda sayfasina hos geldiniz...</h2>");
+    } else if (url === "/contact") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>Iletisim sayfasina hos geldiniz...</h2>");
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h2>404 Sayfa Bulunamadi...</h2>")
+    }
+
+    res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`Sunucu port ${port}'de baslatıldı...`);
+});
